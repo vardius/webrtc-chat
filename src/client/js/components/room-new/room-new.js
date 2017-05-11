7c24ff54d904b62488855f75e225b89e31a393f2
Rafał Lorenz <vardius@gmail.com>
2017-05-12 09:27:18 +1000

    Add avatar img

--- src/client/js/components/room-new/room-new.js
+++ src/client/js/components/room-new/room-new.js
@@ -1,8 +1,7 @@
 import { WebComponent } from 'web-component'
 
 @WebComponent('webrtc-room-new', {
-  template: require('./room-new.html'),
-  styles: require('./room-new.scss')
+  template: require('./room-new.html')
 })
 export class RoomNew extends HTMLElement {
   constructor() {
