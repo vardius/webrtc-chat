0b1fb6310b91f2ca679ce19c622a65634a187dc3
Rafał Lorenz <vardius@gmail.com>
2017-05-11 19:24:57 +1000

    spreat styles among components

--- src/client/js/components/index.js
+++ src/client/js/components/index.js
@@ -1,5 +1,4 @@
 export {Header} from './header/header';
-export {Footer} from './footer/footer';
 export {Chat} from './chat/chat';
 export {Message} from './message/message';
 export {MessageList} from './message-list/message-list';
