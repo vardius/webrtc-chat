94afc0351cfcbe501af9d325b5c99d6513b55eb2
Rafał Lorenz <vardius@gmail.com>
2017-05-23 16:38:09 +1000

    Update favicon

---
+++ bin/chrome.js
@@ -0,0 +1,20 @@
+/**
+ * Listens for the app launching then creates the window
+ *
+ * @see http://developer.chrome.com/apps/app.runtime.html
+ * @see http://developer.chrome.com/apps/app.window.html
+ */
+chrome.app.runtime.onLaunched.addListener(function () {
+  var screenWidth = screen.availWidth;
+  var screenHeight = screen.availHeight;
+
+  chrome.app.window.create('./../build/index.html', {
+    id: "Vardius-WebRTC-Chat",
+    outerBounds: {
+      width: screenWidth,
+      height: screenHeight,
+      left: 0,
+      top: 0
+    }
+  });
+});
